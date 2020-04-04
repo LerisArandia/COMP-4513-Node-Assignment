6e1748899636d592fbfee85555ba8d612e5e714e
LerisArandia <aaran577@mtroyal.ca>
2020-04-03 18:45:15 -0700

    #changed port #switched url fetch

--- client/src/App.js
+++ client/src/App.js
@@ -22,23 +22,23 @@ class App extends React.Component {
 
   async componentDidMount() {
     try {
-       if (JSON.parse(localStorage.getItem("movies"))) {
+      if (JSON.parse(localStorage.getItem("movies"))) {
 
-         this.setState({
-           movies: JSON.parse(localStorage.getItem("movies"))
-         });
-         this.setState({ loaded: true });
-       }
-       else {
-//      const url = "https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?id=ALL";
-           
-           const url = "/api/brief";
+        this.setState({
+          movies: JSON.parse(localStorage.getItem("movies"))
+        });
+        this.setState({ loaded: true });
+      }
+      else {
+        const url = "https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?id=ALL";
 
-      const response = await fetch(url);
-      const jsonData = await response.json();
-      localStorage.setItem("movies", JSON.stringify(jsonData));
-      this.setState({ movies: jsonData, loaded: true });
-       }
+        //  const url = "/api/brief";
+
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        localStorage.setItem("movies", JSON.stringify(jsonData));
+        this.setState({ movies: jsonData, loaded: true });
+      }
 
     } catch (error) {
       console.error(error);
