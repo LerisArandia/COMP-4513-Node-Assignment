dcfb1db1d36fb5712eeced797b430e10fa2ebbaa
nbesh761 <55159411+nbesh761@users.noreply.github.com>
2020-04-06 15:10:16 -0600

    fitler fetch using our api

---
+++ client/src/component/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Title extends React.Component{
+    render(){
+        return(
+            <h2>Go to the movie Detail</h2>
+        )
+    };
+}
+export default Title;
